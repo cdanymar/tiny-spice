add88628c1c50a6fea2a43cd460654ec93788e0b
cdanymar <cdanymar@pm.me>
2026-04-11 12:37:31 +0200

    feat(ui): add toolbar enum

--- src/+UI/ToolBar.m
+++ src/+UI/ToolBar.m
@@ -20,15 +20,15 @@ classdef (Sealed) ToolBar < UI.TinySpiceUI
 
             toolBar.renderGroup(window);
 
-            toolBar.renderButton(window, 0, 'R');
-            toolBar.renderButton(window, 1, 'C');
-            toolBar.renderButton(window, 2, 'L');
-            toolBar.renderButton(window, 3, 'gnd');
-            toolBar.renderButton(window, 4, 'brk');
-            toolBar.renderButton(window, 5, 'U');
-            toolBar.renderButton(window, 6, 'I');
-            toolBar.renderButton(window, 7, '-+-');
-            toolBar.renderButton(window, 8, '');
+            toolBar.renderButton(window, 0, 'R',   'Resistor');
+            toolBar.renderButton(window, 1, 'C',   'Capacitor');
+            toolBar.renderButton(window, 2, 'L',   'Inductor');
+            toolBar.renderButton(window, 3, 'gnd', 'Ground');
+            toolBar.renderButton(window, 4, 'brk', 'Breaker');
+            toolBar.renderButton(window, 5, 'U',   'Voltage Source');
+            toolBar.renderButton(window, 6, 'I',   'Current Source');
+            toolBar.renderButton(window, 7, '-+-', 'Wire');
+            toolBar.renderButton(window, 8, '',    'Inspect and free roam');
         end
     end
 
@@ -52,12 +52,13 @@ classdef (Sealed) ToolBar < UI.TinySpiceUI
 
         % todo use icons instead of text
         % todo use resource manager
-        function renderButton(toolBar, parent, offset, text)
+        function renderButton(toolBar, parent, offset, text, tooltip)
             arguments
                 toolBar UI.ToolBar
                 parent  matlab.ui.Figure
                 offset  int32
                 text    string
+                tooltip string
             end
 
             x = toolBar.Padding + (toolBar.ButtonGap + toolBar.ButtonSize) * offset;
@@ -65,10 +66,14 @@ classdef (Sealed) ToolBar < UI.TinySpiceUI
 
             button = uitogglebutton(toolBar.ToolGroup, ...
                 Text     = text, ...
+                Tooltip  = tooltip, ...
                 Position = [x, y, toolBar.ButtonSize, toolBar.ButtonSize] ...
             );
 
             toolBar.Buttons{end + 1} = button;
         end
+    end
+
+    methods (Access = {?UI.TinySpiceUI})
     end
 end
---
+++ src/+UI/ToolType.m
@@ -0,0 +1,13 @@
+classdef (Sealed) ToolType
+    enumeration
+        PlaceResistor,
+        PlaceCapacitor,
+        PlaceInductor,
+        PlaceGround,
+        PlaceBreaker,
+        PlaceVoltageSource,
+        PlaceCurrentSource,
+        PlaceWire,
+        FreeRoam
+    end
+end
